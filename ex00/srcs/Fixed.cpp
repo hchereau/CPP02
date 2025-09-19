0cefeb348745a07d0b2db303d3babe458ba234f7
Hugo Chereau <hchereau42@gmail.com>
2025-09-19 10:25:27 +0200

    add clean in ex00

--- ex00/srcs/Fixed.cpp
+++ ex00/srcs/Fixed.cpp
@@ -3,32 +3,31 @@
 const int Fixed::_fractionalBits = 8;
 
 Fixed::Fixed() : _value(0) {
-	std::cout << "Default constructor called" << std::endl;
+	std::cout << "Default constructor called" << "\n";
 }
 
-Fixed::Fixed(const Fixed& other) {
-	std::cout << "Copy constructor called" << std::endl;
-	*this = other;
+Fixed::Fixed(const Fixed& other) : _value(other._value) { // la construction par initialisation ne marche que deans les constructeurs
+	std::cout << "Copy constructor called" << "\n";
 }
 
 Fixed& Fixed::operator=(const Fixed& rhs) {
-	std::cout << "Copy assignment operator called" << std::endl;
+	std::cout << "Copy assignment operator called" << "\n";
 	if (this != &rhs)
-		this->_value = rhs.getRawBits();
+		this->_value = rhs._value;
 	return *this;
 }
 
 Fixed::~Fixed() {
-	std::cout << "Destructor called" << std::endl;
+	std::cout << "Destructor called" << "\n";
 }
 
 int Fixed::getRawBits(void) const {
-	std::cout << "getRawBits member function called" << std::endl;
+	std::cout << "getRawBits member function called" << "\n";
 	return this->_value;
 }
 
 void Fixed::setRawBits(int const raw) {
-	std::cout << "setRawBits member function called" << std::endl;
+	std::cout << "setRawBits member function called" << "\n";
 	this->_value = raw;
 }
 
